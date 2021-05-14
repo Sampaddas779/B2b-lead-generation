7c7c44ce3e48553e682a9f2d88372e35afb9feaa
Sampad das <81138758+Sampaddas779@users.noreply.github.com>
2021-05-14 17:34:19 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelector(".first-right-area").addEventListener("click",function(){
+    alert("Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.")
+})
+document.querySelector(".secand-right-area").addEventListener("click",function(){
+    alert("Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.")
+})
+document.querySelector(".last-right-area").addEventListener("click",function(){
+    alert("Click edit button to change this text. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.")
+})
